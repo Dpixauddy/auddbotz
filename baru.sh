a6c73eaad22c9932fdbfb3fefbb92f6ef106cbc9
Dpixauddy <99086561+Dpixauddy@users.noreply.github.com>
2022-02-19 13:45:24 +0700

    Auddyxbotx

---
+++ baru.sh
@@ -0,0 +1,50 @@
+#! /usr/bin/bash
+# hak cipta oleh auddy botz
+#kode warna
+b= ' \033[34;1m '
+g= ' \033[32;1m '
+p= ' \033[35;1m '
+c= ' \033[36;1m '
+r= ' \033[31;1m '
+w= ' \033[37;1m '
+y= ' \033[33;1m '
+#bervariasi _
+sumur=jagreward
+buka = ​​xdg-buka
+# bentuk
+read -p " kamu siapa: " nama ;
+# animasi memuat
+untuk  nomor  dalam {1..10} ;  melakukan
+    tidur 1
+    echo  $nama " kamu ajg.... loading $number % "
+selesai
+# tampilan
+jernih
+echo -e $ g " "
+echo -e $ g " "
+echo -e $ g " "
+gema
+echo -e $y " { " $c " • " $y " } " $c "    Panggilan Spam Oleh auddy botz " $y "    { " $c " • " $y " } "
+echo -e $g " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ "
+echo -e $c " [ " $r " 01 " $c " ] " $b " > " $y " Penulis WhatApp "
+echo -e $c " [ " $r " 02 " $c " ] " $b " > " $y " Berlangganan "
+echo -e $c " [ " $r " 03 " $c " ] " $b " > " $y " Panggilan Spam "
+echo -e $g " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ " $w
+read -p " {•} Pilih: " groks ;
+#basis data
+jika [ $groks  = 1 ] || [ $groks  = 01 ] ;  kemudian
+figlet WhatsApp
+tidur 2
+$buka http://Wa.me/+62881024978351
+panggilan bash.sh
+fi
+jika [ $groks  = 2 ] || [ $groks  = 02 ] ;  kemudian
+berlangganan figlet
+$buka https://youtube.com/channel/UCSJohuQCtqfD2P73Z65g8jg
+panggilan bash.sh
+fi
+jika [ $groks  = 3 ] || [ $groks  = 03 ] ;  kemudian
+echo -e $p " Masukan nomor mulai dari " $c " 8xxxxx " $g
+baca -p " > " nomor
+link= " https://id. $sumur .com/member/verify-mobile/ $nomor "
+curl -s $link
